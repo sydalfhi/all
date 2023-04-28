db410f2b71d6d4b2774a6f06280c34751bc94729
Syaid Alfarishi <syaidalfarishi@gmail.com>
2023-04-28 22:41:23 +0700

    upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('dropdown', () => ({
+//         open: false,
+
+//         toggle() {
+//             this.open = !this.open
+//         }
+//     })) tombol1.addEventListener("click", () => {
+
+const tombol1 = document.querySelector('.tombolRead');
+
+window.addEventListener("load", () => {
+    Swal.fire(
+        'MASIH DALAM PENGEMBANGAN :)',
+        'BENTAR LAGI JUGA BAGUS, xixixixi'
+    )
+})
+// })
